eb123a49636f06c8d8bafc01070394891f4f24a2
milkyKim <kimminji122258@gmail.com>
2020-10-17 21:06:08 +0900

    2차 과제 level3 완료

---
+++ seminar2/hw2/crypto.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+const password = 'qwerty';
+
+const encrypt = (password) => {
+  crypto.randomBytes(64, (err, buf) => {
+    const salt = buf.toString('base64');
+
+    crypto.pbkdf2('비밀번호', salt, 100000, 64, 'sha512', (err, key) => {
+      console.log(`password: ${key.toString('base64')}`);
+
+      const hashed = key.toString('base64');
+
+      fs.writeFile(`${__dirname}/encrypted.txt`, hashed, (err, hashed) => {
+        if (err) return console.log(err.message);
+      });
+    });
+  });
+};
+
+encrypt(password);
