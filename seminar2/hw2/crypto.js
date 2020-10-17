e42d56a21cae966eabf204e71a60e49736d6f3fc
milkyKim <kimminji122258@gmail.com>
2020-10-17 21:09:45 +0900

    2차 과제 level3 완료

--- seminar2/hw2/crypto.js
+++ seminar2/hw2/crypto.js
@@ -7,7 +7,7 @@ const encrypt = (password) => {
   crypto.randomBytes(64, (err, buf) => {
     const salt = buf.toString('base64');
 
-    crypto.pbkdf2('비밀번호', salt, 100000, 64, 'sha512', (err, key) => {
+    crypto.pbkdf2(password, salt, 100000, 64, 'sha512', (err, key) => {
       console.log(`password: ${key.toString('base64')}`);
 
       const hashed = key.toString('base64');
